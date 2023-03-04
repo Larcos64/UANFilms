442c7b0c2c6bbe3e2aaacfc74278b65320f2751b
goslip05 <djimenez750@uan.edu.co>
2023-03-03 23:19:30 -0500

    Revert "Botones y conexion "
    This reverts commit 25e20a4e603e0c9638766bfc27c806f3269b10b9.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,15 +4,15 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" class="text-blue-600 font-bold" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-label for="name" :value="__('Name')" class="font-bold text-blue-600" />
+            <x-text-input id="name" class="block w-full mt-1" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" class="text-blue-600 font-bold" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-label for="email" :value="__('Email')" class="font-bold text-blue-600" />
+            <x-text-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -20,7 +20,7 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" class="text-blue-600 " />
 
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password" class="block w-full mt-1"
                             type="password"
                             name="password"
                             required autocomplete="new-password" />
@@ -32,19 +32,19 @@
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')"  />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+            <x-text-input id="password_confirmation" class="block w-full mt-1"
                             type="password"
                             name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-            <a class="underline text-sm text-violet-600 hover:text-violet-900 font font-bold rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+            <a class="text-sm font-bold underline rounded-md text-violet-600 hover:text-violet-900 font focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
         <div class="flex items-center justify-end mt-4" >
                 {{ __('Already registered?') }}
             </a>
 
-            <x-primary-button class="ml-4 bg-green-400  hover:bg-green-800">
+            <x-primary-button class="ml-4 bg-green-400 hover:bg-green-800">
                 {{ __('Register') }}
             </x-primary-button>
         </div>
