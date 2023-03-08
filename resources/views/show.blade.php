38f6bfe53c6993bd2d31a156d7d0b3a1aee44f86
goslip05 <djimenez750@uan.edu.co>
2023-03-07 23:26:02 -0500

    Update show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -14,7 +14,7 @@
                 <img src="{{'https://image.tmdb.org/t/p/w500/'.$detallesPelicula['poster_path']}}" alt="" class="w-64 lg:w-96" style="width: 24rem" id="bright">
             </div>          
             <div class="md:ml-24">
-                <h2 class="text-4xl font-semibold">{{$detallesPelicula['title']}}(2012)</h2>
+                <h2 class="text-4xl font-semibold">{{$detallesPelicula['title']}}</h2>
                 <div class="flex flex-wrap items-center mt-1 text-sm text-gray-300">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-orange-500 fill-current">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
@@ -53,7 +53,7 @@
                         <a 
                             Target="_blank"
                             href="https://youtube.com/watch?v={{ $detallesPelicula['videos']['results'][0]['key']}}"
-                            class=" inline-flex items-center px-3 py-3 font-semibold text-gray-900 transition duration-150 ease-in-out bg-orange-500 rounded hover:bg-orange-600">
+                            class="inline-flex items-center px-3 py-3 font-semibold text-gray-900 transition duration-150 ease-in-out bg-orange-500 rounded  hover:bg-orange-600">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 fill-current">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
                             </svg>
